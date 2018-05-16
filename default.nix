e4c01ecb8db96a6505fc36faa2be7ff80ed7303f
Jörg Thalheim <joerg@thalheim.io>
2018-05-16 10:57:03 +0100

    update default.nix

--- default.nix
+++ default.nix
@@ -11,14 +11,22 @@ let
     };
   });
 
-  myPerf = linuxPackages.perf.overrideAttrs(old: {
-    patches = [ ./perf-script-sample-addr.patch ];
+  myLinuxPackages = linuxPackages;
+  myLinux = linux;
+
+  myPerf = myLinuxPackages.perf.overrideAttrs(old: {
+    patches = [ 
+      ./perf-script-sample-addr.patch
+      # kernel source path
+      (fetchpatch {
+        url = "https://github.com/Mic92/linux/commit/fdf56d6b10f545f25cae757c8fbeb08cb9d396c3.patch";
+        sha256 = "1qc50wagz3snsb6ql3wjgnyqf8lpyzqzrg7nvgk97965cgmly8vy";
+      })
+    ];
+    NIX_CFLAGS_COMPILE = " -DKERNELDIR=\"${myLinux.dev}/\" ";
     makeFlags = [ "LIBCLANGLLVM=1" ];
 
     preBuild = (old.preBuild or "") + ''
-      # set by llvm -> not recognized by g++
-      unset NIX_CXXSTDLIB_LINK
-
       # perf assumes that clang + llvm are in the same project
       EXTRA_PERFLIBS+=" -Wl,--start-group"
       for lib in ${llvmPackages_39.clang-unwrapped}/lib/*.a; do
@@ -39,8 +47,9 @@ let
   });
 
 in stdenv.mkDerivation {
-  name = "env";
+  name = "angr-deps";
   buildInputs = [
+    linuxPackages.bcc
     bashInteractive
     #pypy27
     unicorn-emu
@@ -51,10 +60,11 @@ in stdenv.mkDerivation {
     python2Packages.pandas
     qt5.qttools
     gdb
-    # avoid polluting PYTHONPATH
     (buildEnv {
-      name = "python-tools";
-      paths = [ mypy python2Packages.virtualenv ]; 
+      name = "tools";
+      # here we only want binaries without polluting
+      # PYTHONPATH/NIX_CFLAGS_COMPILE...
+      paths = [ mypy python2Packages.virtualenv python2Packages.yapf musl.dev ]; 
     })
     myPerf
   ];
@@ -63,4 +73,6 @@ in stdenv.mkDerivation {
   SOURCE_DATE_EPOCH="1523278946";
   # better not to use tmpfs
   TMPDIR="/tmp";
+
+  hardeningDisable = ["all"];
 }
