71838b6c224718aac9de738cf9fe85567c0a8165
Jörg Thalheim <joerg@thalheim.io>
2018-05-25 00:24:20 +0100

    add type for main function

--- hase/__init__.py
+++ hase/__init__.py
@@ -2,12 +2,14 @@ from __future__ import absolute_import, division, print_function
 from .cli import parse_arguments
 import sys
 
+from typing import List
+
 
 def main(argv=sys.argv):
+    # type: (List[str]) -> None
     args = parse_arguments(argv)
     if args.debug:
         import pry
         with pry:
             return args.func(args)
-    else:
-        return args.func(args)
+    return args.func(args)
