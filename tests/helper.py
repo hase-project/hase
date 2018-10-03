d0aa0d7339f9b7866671531b84538c3939955e84
Jörg Thalheim <joerg@thalheim.io>
2018-10-03 13:43:58 +0100

    fix loopy test

--- tests/helper.py
+++ tests/helper.py
@@ -6,3 +6,4 @@ from hase.path import Path
 
 TEST_ROOT = Path(os.path.dirname(os.path.realpath(__file__)))
 TEST_BIN = Path(str(TEST_ROOT.join("bin")))
+TEST_TRACES = Path(str(TEST_ROOT.join("traces")))
--- tests/test_replay.py
+++ tests/test_replay.py
@@ -1,32 +1,27 @@
 from __future__ import absolute_import, division, print_function
 
+import logging
+
 import nose
 
 from hase import main
 
-import logging
-
-from .helper import TEST_BIN
-
-
-def argc(simstate):
-    """
-    argc is on the stack in main/_start for both programs:
-    """
-    # equivalent (uint64_t)*rsp
-    s = simstate
-    return s.mem[s.solver.eval(s.regs.rsp)].uint64_t.concrete
+from .helper import TEST_TRACES
 
 
 def test_loopy():
     state = main([
         "hase", "replay",
-        str(TEST_BIN.join("loopy", "loopy-20180514T145114.tar.gz"))
+        str(TEST_TRACES.join("loopy-20181003T123336.tar.gz"))
     ])
     last_state = state[-1]
     # We called loopy with 6 arguments:
     # ./loopy a b c d e
-    nose.tools.eq_(argc(last_state.simstate), 6)
+    s = last_state[0].simstate
+    # loopy does not touch rsp so we can get the location of argc by dereferencing 
+    # the top of the stack
+    argc = s.mem[s.solver.eval(s.regs.rsp)].uint64_t.concrete
+    nose.tools.eq_(argc, 6)
 
 
 def test_control_flow():
