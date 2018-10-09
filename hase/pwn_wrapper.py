ed84408639228f77228e1e16553db5bc20610048
Jörg Thalheim <joerg@thalheim.io>
2018-10-10 00:03:15 +0100

    pwn_wrapper: environment variables must be set before import
    revert from previous commit

--- hase/pwn_wrapper.py
+++ hase/pwn_wrapper.py
@@ -2,8 +2,7 @@ from __future__ import absolute_import, division, print_function
 
 import os
 
-from pwnlib.elf.corefile import Coredump, Mapping
-from pwnlib.elf.elf import ELF
-
 # stop pwnlib from doing fancy things
 os.environ["PWNLIB_NOTERM"] = "1"
+from pwnlib.elf.corefile import Coredump, Mapping
+from pwnlib.elf.elf import ELF
