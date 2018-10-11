8a34003ad3491ab3c6db2a390ddec27a23b0d222
Jörg Thalheim <joerg@thalheim.io>
2018-10-11 11:35:34 +0100

    setup.py: make pwnlib stop fucking up the terminal

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     packages=find_packages(),
     install_requires=[
       'angr @ https://github.com/hase-project/angr/archive/5219e2457c95ae8db6b3fa2897076d268fb356e5.zip',
-      'pwntools @ https://github.com/Mic92/pwntools/archive/25563ef382fbe4f5e5f9865aa6e08376cd4dab8d.zip',
+      'pwntools @ https://github.com/Mic92/pwntools/archive/eabc50d0930bdb3918db814d2245e61b4a57079d.zip',
       'monkeyhex',
       'qtconsole',
       'pry.py',
