0272d1f297ae2305f108a7cfc32a0e2f0a1bd3bc
Hao Lu <harveylu@umich.edu>
2018-11-28 01:39:39 -0500

    Support for measuring/recording the rusage of a process by its id after launching.

--- tests/applications/measure_time.py
+++ tests/applications/measure_time.py
@@ -1,28 +1,36 @@
 """
     Use this script to monitor a running process.
 """
+import argparse
 import subprocess
 import time
-import argparse
 
 
 def main():
+    result = None
     while True:
         time.sleep(1)
-        output = subprocess.getoutput(f'ps -jH -p {args.pid} --noheaders')
-        if not output:
+        status, output = subprocess.getstatusoutput(f'cat /proc/{args.pid}/stat')
+        if status:
             break
 
-        if args.file:
-            with open(args.file, 'w') as file:
-                file.write(output)
-        else:
-            print(output.strip())
+        result = output
+
+    if args.file:
+        with open(args.file, 'w') as file:
+            file.write(result)
+    else:
+        print(result.strip())
+        result = result.strip().split()
+        print(float(result[13]) + float(result[14]))
+
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("pid", type=int, help="The pid to monitor usage")
-    parser.add_argument("file", default=None, help="The file to output the result")
+    parser.add_argument(
+        "-f", "--file", default=None, help="The file to output the result"
+    )
     args = parser.parse_args()
     main()
