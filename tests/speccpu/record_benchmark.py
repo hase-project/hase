b1eb4e7ac8f60e9aaa1a995631eb3bac250cc95a
Jörg Thalheim <joerg@thalheim.io>
2018-12-04 16:48:47 +0100

    reformat tests

--- tests/speccpu/record_benchmark.py
+++ tests/speccpu/record_benchmark.py
@@ -9,16 +9,19 @@ import argparse
 
 from config import *
 
+
 def main():
     subprocess.run(args.args, shell=True)
     rusage = tuple(resource.getrusage(resource.RUSAGE_CHILDREN))
-    with open(USAGE, 'w') as file:
-        file.write(', '.join([str(x) for x in rusage]))
-        file.write('\n')
+    with open(USAGE, "w") as file:
+        file.write(", ".join([str(x) for x in rusage]))
+        file.write("\n")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "args", nargs="*", help="Executable and arguments for perf tracing")
+        "args", nargs="*", help="Executable and arguments for perf tracing"
+    )
     args = parser.parse_args()
     main()
