7ba4b13ee82a6c68a6cf6507a139d081b2fcd4aa
Jörg Thalheim <joerg@thalheim.io>
2018-12-10 18:45:56 +0100

    tooling to update pinnings

--- setup.py
+++ setup.py
@@ -10,8 +10,10 @@ setup(
     install_requires=[
         "angr @ https://github.com/hase-project/angr/archive/e1b66e2eebe0756ad5a06a686d8a086528ad7cf6.zip",
         "cle @ https://github.com/angr/cle/archive/cd39642d6532ac2908ab6aa510240e9397341c92.zip",
-        # remove this after the next archinfo update: https://github.com/hase-project/angr/pull/4#issuecomment-445861599
+        "claripy @ https://github.com/angr/claripy/archive/ca524cb2af19952d247ff072b38140b4eb8d21c1.zip",
+        "pyvex @ https://github.com/angr/pyvex/archive/44a8f41d960e108610144335dac4f852e050e2e7.zip",
         "archinfo @ https://github.com/angr/archinfo/archive/d3eb03b047847d55fec71c06e9d2a15bc2f28d7d.zip",
+
         "pwntools @ https://github.com/hase-project/pwntools/archive/74a98908a19e00df399abd4b8e956abeabbd62ae.zip",
         "monkeyhex",
         "ipython",
